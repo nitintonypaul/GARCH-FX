6d008d8ffdfb7e95b77c95a5e04990cf2d4fab7a
Nitin Tony Paul <108007300+nitintonypaul@users.noreply.github.com>
2025-07-05 11:15:28 +0530

    Add files via upload

--- prog2.py
+++ prog2.py
@@ -1,11 +1,16 @@
 import numpy as np
 from arch import arch_model
 import yfinance as yf
+import matplotlib.pyplot as plt
 
+# Constants
 ANNUALIZER = np.sqrt(252)
-FORECASTS = 100
+FORECASTS = 1000
+THETA = 0.01 # Can be calibrate for regime switching
+DELTA = 1 # Can be modelled using entropy-change and sigmoid
+ticker = 'AAPL'
 
-AAPL = yf.Ticker('AAPL').history(period="180d")["Close"]
+AAPL = yf.Ticker(ticker).history(period="180d")["Close"]
 AAPLreturns = np.log(AAPL / AAPL.shift(1)).dropna()
 AAPLreturns *= 100
 model = arch_model(AAPLreturns, vol='Garch', p=1, q=1, dist='normal')
@@ -27,20 +32,30 @@ for i in range(FORECASTS):
 # Possible regime shift
 GARCHsampled = []
 previousSampledVariance = VOLATILITY ** 2
-THETA = 0.01
 GARCHsampled.append(VOLATILITY / 100)
 for i in range(FORECASTS):
-    
-    # Passing Variance as mode
-    shape_k = previousSampledVariance / THETA + 1
 
+    # Passing Variance as mode
+    # DELTA is decaying constant
+    shape_k = (DELTA * previousSampledVariance) / THETA + 1
+
+    # Sampling from gamma distribution
     sample = np.random.gamma(shape=shape_k, scale=THETA, size=100)
+
+    # GARCH forecast
     newSampledVariance = OMEGA + (ALPHA + BETA) * np.mean(sample)
-    GARCHsampled.append(np.sqrt(newSampledVariance) / 100)
     previousSampledVariance = newSampledVariance
+    
+    GARCHsampled.append(np.sqrt(previousSampledVariance) / 100)
 
 for NG, SG in zip(GARCHforecast, GARCHsampled):
-    print(f"NG: {NG:.6f}    SG: {SG:.6f}")
+    print(f"NG: {NG:.6f}    GD-G: {SG:.6f}")
 
 print("ANNUALIZED GARCH:", np.sqrt(previousVariance) / 100 * ANNUALIZER)
 print("ANNUALIZED GARCH SAMPLED:", np.sqrt(previousSampledVariance) / 100 * ANNUALIZER)
+
+plt.plot(GARCHforecast, label="GARCH")
+plt.plot(GARCHsampled, label="GD-GARCH")
+plt.legend()
+plt.grid(True)
+plt.show()
