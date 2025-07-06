dd6340139b0cd69c25b9b598d856d4a035c9df6d
Nitin Tony Paul <108007300+nitintonypaul@users.noreply.github.com>
2025-07-07 00:03:48 +0530

    adding heston & regime shifting

--- garchfx.py
+++ garchfx.py
@@ -1,21 +1,54 @@
 import numpy as np
 from arch import arch_model
+import sys
+
+GLOBAL_SEED = None
 
 # GARCH-FX forecasting
 # Takes final conditional volatility, forecast horizon, GARCH parameters, delta and theta
 # delta can be tweaked for regime shifting function
-def fxforecast(volatility, nahead, params, delta, theta):
+def fxforecast(volatility, nahead, params, delta, theta, reg=False, regimeStates=None, regimes=None):
+
+    # Regime switching function
+    def regimeswitcher(delta, regimeStates, regimes):
+
+        
+        # Basic static Markov chain of probabilities per regime
+        if regimeStates == None:
+            regimeStates = np.array([[0.88, 0.10, 0.02], [0.06, 0.89, 0.05], [0.02, 0.33, 0.65]])
+
+        if regimes == None:
+            regimes = [0.7, 1, 1.3]
+        
+        if len(regimeStates) != len(regimes):
+            print("Invalid regime input")
+            sys.exit(0)
+
+        currentRegime = regimes.index(delta)
+
+        # Generating random number for a chance to switch regimes
+        r = np.random.rand()
+        probs = regimeStates[currentRegime]
+        new_regime = regimes[np.searchsorted(np.cumsum(probs), r)]
+    
+        # returning a new regime (might be same)
+        return new_regime
+
     
     forecasts = []
     forecasts.append(volatility / 100)
     previousVariance = volatility ** 2
     OMEGA, ALPHA, BETA = params[0], params[1], params[2]
+    np.random.seed(GLOBAL_SEED)
 
     for i in range(nahead):
-
+        
         # Modelling shape to fit variance as the mode
         SHAPE = (previousVariance / theta) + 1
 
+        if reg:
+            delta = regimeswitcher(delta, regimeStates, regimes)
+        
         # Sampling from gamma distribution
         stochasticVariance = np.random.gamma(shape=SHAPE, scale=theta, size=1)[0]
 
@@ -48,9 +81,9 @@ def garchforecast(volatility, nahead, params):
     
     return np.array(GARCHforecasts)
 
+# Function to obtain final conditional volatility and parameters
 def getGARCHdata(returns):
-    # Using the normal GARCH model from arch package
-    # GARCH(1,1) is used to compare
+
     model = arch_model(returns, vol='Garch', p=1, q=1, dist='t')
     results = model.fit(disp='off')
 
@@ -59,3 +92,38 @@ def getGARCHdata(returns):
     VOLATILITY = np.sqrt(results.forecast(horizon=1).variance.values[-1, :][0])
 
     return VOLATILITY, [ALPHA, BETA, OMEGA]
+
+# Function to get realized volatility
+def realVol(returns, steps):
+
+    # Assigning a small np array
+    volarr = np.zeros(1)
+
+    # Find each volatility
+    for i in range(1000, 1000+steps):
+        partial = returns[:i][-180:]
+        volarr = np.append(volarr, np.std(partial))
+    
+    # Deleting the first element (0) and returning
+    return np.delete(volarr, 0)
+
+# Heston model function to model volatility
+def heston(kappa, theta, v0):
+    
+    # Heston parameters
+    sigma = 0.6
+    T = 2.75          
+    N = 1001       
+    dt = T / N
+
+    # Simulate
+    np.random.seed(GLOBAL_SEED)
+    vt = np.zeros(N)
+    vt[0] = v0
+
+    for t in range(1, N):
+        z = np.random.normal()
+        vt[t] = vt[t-1] + kappa * (theta - vt[t-1]) * dt + sigma * np.sqrt(max(vt[t-1], 0)) * np.sqrt(dt) * z
+        vt[t] = max(vt[t], 0)
+
+    return np.sqrt(vt)
--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import numpy as np
 import yfinance as yf
 import matplotlib.pyplot as plt
+import garchfx as gfx
 from garchfx import fxforecast, garchforecast, getGARCHdata
 
 # Constants
 FORECASTS = 1000
-ticker = 'AAPL'
+TICKER = 'GLD'
 
 # Defining variables of this GARCH implementation
 # SCALE controls the volatility wiggliness, i.e. volatility of volatility
 # SCALE → 0 => Volatility models closer to GARCH volatility, OR the long term variance 
 # SCALE → 1 (or infinity) => Heavy volatility spikes are observed
+# SCALE = 1e-3 seems ideal
 SCALE = 1e-3
 
 # DELTA is the regime shift variable. 
@@ -21,27 +23,32 @@ DELTA = 1
 
 # Fetching data using yfinance
 # logReturns are scaled by 100 for better optimizer performance. It is scaled back by 100 whenever necessary
-data = yf.Ticker(ticker).history(period="1000d")["Close"]
-logReturns = np.log(data / data.shift(1)).dropna()
+data = yf.Ticker(TICKER).history(period="2000d")["Close"]
+realizedVolatility = 100 * gfx.realVol(np.log(data / data.shift(1)).dropna(), FORECASTS)
+volatilityData = data[-1000:]
+logReturns = np.log(volatilityData / volatilityData.shift(1)).dropna()
 logReturns *= 100
 
 # Obtaining GARCH conditional volatility and parameters
 VOLATILITY, PARAMS = getGARCHdata(logReturns)
 
+# Heston forecast
+speed = 1 - PARAMS[1] - PARAMS[2]
+variance = PARAMS[0]/speed
+heston = gfx.heston(speed, variance, VOLATILITY**2)
+
 # manual GARCH to forecast in percentages
 GARCHforecast = 100 * garchforecast(VOLATILITY, nahead=FORECASTS, params=PARAMS)
 
 # GARCH-FX stochastic forecasting extension in percentages
-bigFX = 100 * fxforecast(VOLATILITY, nahead=FORECASTS, params=PARAMS, delta=DELTA, theta=1e-2)
-FXforecast = 100 * fxforecast(VOLATILITY, nahead=FORECASTS, params=PARAMS, delta=DELTA, theta=SCALE)
-smallFX = 100 * fxforecast(VOLATILITY, nahead=FORECASTS, params=PARAMS, delta=DELTA, theta=1e-4)
+FXforecast = 100 * fxforecast(VOLATILITY, nahead=FORECASTS, params=PARAMS, delta=DELTA, theta=SCALE, reg=True)
 
 # Plotting Values
+plt.plot(realizedVolatility, label="Realized Vol.")
 plt.plot(GARCHforecast, label="GARCH", linewidth=3)
-plt.plot(bigFX, label=f"S-GARCH @ 0.01", alpha=0.4)
-plt.plot(FXforecast, label=f"S-GARCH @ {SCALE}", alpha=0.6)
-plt.plot(smallFX, label="S-GARCH @ 0.0001", alpha=0.8)
-plt.title("AAPL - GARCH forecast vs GARCH-FX")
+plt.plot(FXforecast, label=f"GARCH-FX @ {SCALE}", alpha=0.8)
+plt.plot(heston, label="Heston", alpha=0.8)
+plt.title(f"{TICKER} - GARCH forecast vs GARCH-FX")
 plt.xlabel("Forecasted Days")
 plt.ylabel("Daily Volatility (%)")
 plt.legend()
